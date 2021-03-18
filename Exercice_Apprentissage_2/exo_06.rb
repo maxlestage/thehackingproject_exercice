808fea534a735ee5f358e5f1f07ba59d9a856597
maxlestage <maxlestage@icloud.com>
2021-03-18 16:55:34 +0100

    sixth  exercices done

---
+++ Exercice_Apprentissage_2/exo_06.rb
@@ -0,0 +1,2 @@
+# 6. Un programme qui répète (bis)
+# Écris un programme exo_06.rb qui demande un nombre à un utilisateur, puis qui écrit autant de fois -1 "Bonjour toi !". Ainsi, si l'utilisateur rentre 10, le programme devra écrire 9 fois "Bonjour toi !"
